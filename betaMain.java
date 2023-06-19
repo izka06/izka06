6d3d047f8d6ab775512d574d17264cb10dc55403
unknown <wwx13685@student.wsb.warszawa.pl>
2023-06-19 22:06:20 +0200

    Dodanie plików

---
+++ betaMain.java
@@ -0,0 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Powitanie z nazwą użytkownika
+        String username = System.getProperty("user.name");
+        System.out.println("Witaj, " + username);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        //Wypisywanie aktualnej godziny
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        System.out.println("Aktualna data i godzina: " + formattedDateTime);
+
+    }
+}
