3ceb42a4955ca084bcef5e2a49950f20dd32f891
unknown <wwx13685@student.wsb.warszawa.pl>
2023-06-19 22:19:10 +0200

    Zakomentowanie try catch

--- test.java
+++ test.java
@@ -11,10 +11,10 @@ public class Main {
 
             System.out.print("Aktualny czas: " + formattedTime);
 
-            try {
+            /*try {
                 Thread.sleep(1000); //1 sekunda
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
     }
 }
