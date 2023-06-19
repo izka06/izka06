f8f21fb2be9b530bf5d6c77c6f700d1737f15346
unknown <wwx13685@student.wsb.warszawa.pl>
2023-06-19 21:58:00 +0200

    Dodanie nowych plików

---
+++ alphaMain.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Witaj w testowym pliku!");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+        //Wypisywanie aktualnej godziny
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formattedDateTime = currentDateTime.format(formatter);
+        System.out.println("Aktualna data i godzina: " + formattedDateTime);
+
+    }
+}
